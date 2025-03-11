aa734a9adbb7ad1c8e96ee8cf2e331da3e031ee3
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-03-11 11:07:31 +0100

    Create only one user, with a public ssh key

--- hpc_provisioner/src/hpc_provisioner/utils.py
+++ hpc_provisioner/src/hpc_provisioner/utils.py
@@ -1,5 +1,7 @@
 import os
 
+from cryptography.hazmat.primitives import serialization
+
 
 def get_sbonexusdata_bucket():
     return os.environ.get("SBO_NEXUSDATA_BUCKET")
@@ -11,3 +13,15 @@ def get_containers_bucket():
 
 def get_scratch_bucket():
     return os.environ.get("SCRATCH_BUCKET")
+
+
+def generate_public_key(key_material):
+    private_key = serialization.load_pem_private_key(key_material.encode(), password=None)
+    public_key = private_key.public_key()
+    public_key.public_bytes(
+        encoding=serialization.Encoding.OpenSSH, format=serialization.PublicFormat.OpenSSH
+    )
+    public_bytes = public_key.public_bytes(
+        encoding=serialization.Encoding.OpenSSH, format=serialization.PublicFormat.OpenSSH
+    )
+    return public_bytes.decode("utf-8")
