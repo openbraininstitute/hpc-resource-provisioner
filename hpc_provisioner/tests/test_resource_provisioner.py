0d72be2270dadc8d4924734c538c5ab5280042d0
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-04-29 15:19:35 +0200

    Fix failing tests

--- hpc_provisioner/tests/test_resource_provisioner.py
+++ hpc_provisioner/tests/test_resource_provisioner.py
@@ -164,28 +164,74 @@ def test_post(patched_boto3, post_event, key_exists):
     patched_boto3.client.return_value = mock_client
     with patch("hpc_provisioner.handlers.create_keypair") as patched_create_keypair:
         if key_exists:
-            patched_create_keypair.return_value = {
-                "KeyName": test_cluster_name,
-            }
+            patched_create_keypair.side_effect = [
+                {
+                    "KeyName": test_cluster_name,
+                },
+                {"KeyName": f"{test_cluster_name}_sim"},
+            ]
         else:
-            patched_create_keypair.return_value = {
-                "KeyMaterial": "secret_stuff",
-                "KeyName": test_cluster_name,
-            }
+            patched_create_keypair.side_effect = [
+                {"KeyMaterial": "secret-stuff", "KeyName": test_cluster_name},
+                {
+                    "KeyMaterial": """-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAvfvswsbNBM05kutLby0DZEl+tWx62yqMU0IgKqEoPamtgS3s
+V6S5xOLqYItd5UAMLf4pAGbvqXNy8BbVNmiKfPEXRfBq1hC4t/FmILIh5uyTRqEe
+ocB9s/RIR/bkAdrhK60ZzwPD4RHqfShGE6FuB3VPHbxWVsfrYjjsC66n675qSdnk
+/4N69aURUX3xB+Rco2i1RW3VUbylPXnbKlG1KtIYGp6ctv3vQf3zXR7snp4580mu
+1T6DBSmdhNY7r/IUGcQTePNo+R89FaK0OEOVNuPJUsT8InEihmTxR8HTkS6x31kd
+OW9uWyCjUhZ86JgYMIWInOuD6dVjiVfOS3lz4wIDAQABAoIBAA6TWWLIJcqMhDJF
+YxAwf+YdzV7V58cQyJtKo3Uh0BiLAAi9ITjIJoh4cErf5mBEPar5qOOjAhgaB2Ns
+HQeDlbxoMsTm4QtzVPinyJIRmJBC7jmo+tSmE/7Oaw06X9vUkxnqueQsAccuvLLF
+eDrhU7O2yE8bt/QIeoKao2FyEOgBHf5REK97PpXiuKC1YrZ4xf1akQJeKXOuVGMc
+J373BnbiM2vAuB9nZxIwMqpALEV5Rd5kI/pjZViAMqZiZb5zworncdL05mRn9M9S
+nIV8/lN2wvtiyT8fyapcY9nqYxTOwR6PrjM9xPWX8eehnR+357IoPPsxCVpd3Npt
+r2YbhyECgYEA5vOejzbmVQSYfc46g0WQgvfwMvfUPGtl1mvakgdiM4TlxzfrTUxO
+3Tt0ClP5eO6szp26U/8RMlmwCGSZeR+hp5ATu/KpElWOJESAeuMRkPzsebGwpqHU
+g8FYBhbCZlxBP1VBtu5HVMzPTlzs1nWLERuRoKSjzbvqQy8udowv5f0CgYEA0pbW
++1S8M+/CuTgCscnjnEKOyo3/o821GUjwacRPJ1Rs6VO5bSUOdfD87zl5Bw2MO+YO
+UVbr9DazDshoIglqPPMVUedMGkoKaoVm3XovLLs8Bf+w6nvf/cTyIG7L2Zg4S8bx
+4zEW+3JezIcB5exhKzpt8SnezJy/yV/wOZ28918CgYAyhf8c83SmCrBVbUUtrI01
+qYnZjI/Ye+I2azfQlF7uSFeAIoKOUXA7Q6NaEw7TXttdA/JcJ8OaUTaKT3+nmLzj
+jEhU6HwGL8M9ueKlf4E7R6lv4eh0O5jjDev0wQvcGriHY15R54ShT4DWcsu5CtPW
+dUKBcyMGgeJ4uhyfAIIF/QKBgC61VimYucWrQD1ktvRIGzvlZ1Z1+GWUkr5w9yH+
+cLAAgcee0lnrBjISqYdF8BooXxpKBJL1/I/GMiDtQmuKOw4ZvHjWHIMYOQc3X4Fw
+QFZjkQSjmdHod94JeMrIyF4S7SmhjrUdhkNvMqeaqkkdDNBRvWDoMIqhmchIhzfA
+TNFxAoGATB6ReLKjX8kudSzGKQEYiMN9afFCYt4UsoED/FPoOTu7K9kuZ7Sbh/Q3
+WoeXwwyBA7QuulZo9ACvHw6PqHbqQqD8IqJVocYisq9EmGFSnAmUYIX7hBwWr7pP
+e15Cgo+/r/nqbT21oTkp4rbw5nT9lVyuHyBralzJ7Q/BDXXY0v0=
+-----END RSA PRIVATE KEY-----""",
+                    "KeyName": f"{test_cluster_name}_sim",
+                },
+            ]
 
         with patch("hpc_provisioner.handlers.store_private_key") as patched_store_private_key:
-            patched_store_private_key.return_value = {"ARN": "secret ARN"}
+            patched_store_private_key.side_effect = [
+                {"ARN": "secret ARN"},
+                {"ARN": "secret ARN sim"},
+            ]
             actual_response = handlers.pcluster_create_request_handler(post_event)
+    expected_args = {
+        "vlab_id": post_event["vlab_id"],
+        "project_id": post_event["project_id"],
+        "keyname": f"pcluster-{post_event['vlab_id']}-{post_event['project_id']}",
+        "options": {
+            "dev": False,
+            "include_lustre": None,
+            "tier": None,
+            "project_id": post_event["project_id"],
+            "vlab_id": post_event["vlab_id"],
+            "keyname": None,
+            "sim_pubkey": None,
+        },
+    }
+    if not key_exists:
+        expected_args["sim_pubkey"] = (
+            """ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9++zCxs0EzTmS60tvLQNkSX61bHrbKoxTQiAqoSg9qa2BLexXpLnE4upgi13lQAwt/ikAZu+pc3LwFtU2aIp88RdF8GrWELi38WYgsiHm7JNGoR6hwH2z9EhH9uQB2uErrRnPA8PhEep9KEYToW4HdU8dvFZWx+tiOOwLrqfrvmpJ2eT/g3r1pRFRffEH5FyjaLVFbdVRvKU9edsqUbUq0hganpy2/e9B/fNdHuyenjnzSa7VPoMFKZ2E1juv8hQZxBN482j5Hz0VorQ4Q5U248lSxPwicSKGZPFHwdORLrHfWR05b25bIKNSFnzomBgwhYic64Pp1WOJV85LeXPj"""
+        )
     mock_client.invoke_async.assert_called_with(
         FunctionName="hpc-resource-provisioner-creator",
-        InvokeArgs=json.dumps(
-            {
-                "vlab_id": post_event["vlab_id"],
-                "project_id": post_event["project_id"],
-                "keyname": f"pcluster-{post_event['vlab_id']}-{post_event['project_id']}",
-                "options": {},
-            }
-        ),
+        InvokeArgs=json.dumps(expected_args),
     )
     expected_response = expected_response_template(
         text=json.dumps(
@@ -194,7 +240,10 @@ def test_post(patched_boto3, post_event, key_exists):
                     "clusterName": test_cluster_name,
                     "clusterStatus": "CREATE_REQUEST_RECEIVED",
                     "private_ssh_key_arn": "secret ARN",
-                }
+                    "ssh_user": "sim",
+                },
+                "admin_user_private_ssh_key_arn": "secret ARN",
+                "private_ssh_key_arn": "secret ARN sim",
             }
         )
     )
@@ -229,7 +278,7 @@ def test_delete(
     expected_response = expected_response_template(text=json.dumps(data["deletingCluster"]))
     assert actual_response == expected_response
     patched_get_registered_subnets.assert_called_once()
-    patched_remove_key.assert_called_once()
+    assert patched_remove_key.call_count == 2
     call1 = call(mock_client, "subnet-123")
     call2 = call(mock_client, "subnet-234")
     patched_free_subnet.assert_has_calls([call1, call2], any_order=True)
@@ -272,7 +321,7 @@ def test_delete_not_found(patched_remove_key, patched_dynamodb_client, delete_ev
         delete_cluster.assert_called_once()
     assert result == {"statusCode": 404, "body": error_message}
     patched_dynamodb_client.assert_called_once()
-    patched_remove_key.assert_called_once()
+    assert patched_remove_key.call_count == 2
 
 
 @patch(
@@ -288,7 +337,7 @@ def test_delete_internal_server_error(patched_remove_key, patched_dynamodb_clien
         patched_delete_cluster.assert_called_once()
     assert result == {"statusCode": 500, "body": "<class 'RuntimeError'>"}
     patched_dynamodb_client.assert_called_once()
-    patched_remove_key.assert_called_once()
+    assert patched_remove_key.call_count == 2
 
 
 @patch("hpc_provisioner.pcluster_manager.pc.create_cluster")
