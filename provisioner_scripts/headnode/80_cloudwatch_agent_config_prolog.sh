344177fc73db4302fb2d6ae2b8a204dc715a4550
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-06-04 11:30:34 +0200

    End-users won't have the weji subfolder; they'll already be in their own subfolder

--- provisioner_scripts/headnode/80_cloudwatch_agent_config_prolog.sh
+++ provisioner_scripts/headnode/80_cloudwatch_agent_config_prolog.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CWAGENT_CONFIG=/sbo/data/scratch/weji/CWAgent_config_$SLURM_CLUSTER_NAME.json
+CWAGENT_CONFIG=/sbo/data/scratch/CWAgent_config_$SLURM_CLUSTER_NAME.json
 
 if [ ! -f $CWAGENT_CONFIG ]; then
 	echo "Create CWAGENT_CONFIG " $CWAGENT_CONFIG
-	sed "s/\$CLUSTER_NAME/$SLURM_CLUSTER_NAME/g" /sbo/data/scratch/weji/CWAgent_config_tpl.json > $CWAGENT_CONFIG
-fi	
+	sed "s/\$CLUSTER_NAME/$SLURM_CLUSTER_NAME/g" /sbo/data/scratch/CWAgent_config_tpl.json > $CWAGENT_CONFIG
+fi
 
 #sudo /opt/slurm/bin/srun --ntasks=$NODES --ntasks-per-node=1 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a stop
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/$CWAGENT_CONFIG -s
