5592e455916e1b46d26d78c98a011e6d9e985f2b
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-06-05 11:27:52 +0200

    The cluster has no internet access, write the prolog script

---
+++ provisioner_scripts/80_cloudwatch_agent_config_prolog.sh
@@ -0,0 +1,13 @@
+cat << _EOF_ >> /opt/slurm/etc/scripts/prolog.d/80_cloudwatch_agent_config_prolog.sh
+#!/bin/bash
+
+CWAGENT_CONFIG=/sbo/data/scratch/CWAgent_config_\$SLURM_CLUSTER_NAME.json
+
+if [ ! -f \$CWAGENT_CONFIG ]; then
+	echo "Create CWAGENT_CONFIG " \$CWAGENT_CONFIG
+	sed "s/\\$CLUSTER_NAME/\$SLURM_CLUSTER_NAME/g" /sbo/data/scratch/CWAgent_config_tpl.json > \$CWAGENT_CONFIG
+fi
+
+#sudo /opt/slurm/bin/srun --ntasks=\$NODES --ntasks-per-node=1 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a stop
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/\$CWAGENT_CONFIG -s
+_EOF_
