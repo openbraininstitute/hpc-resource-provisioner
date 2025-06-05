661e5751bd3c6c385fc4b465f1e821b1e0b6b55a
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-06-05 11:27:52 +0200

    The cluster has no internet access, write the prolog script

--- provisioner_scripts/environment.sh
+++ provisioner_scripts/environment.sh
@@ -3,5 +3,3 @@
 export CLUSTER_NAME=$1
 
 echo "CLUSTER_NAME=${CLUSTER_NAME}" >> /etc/environment
-
-curl --create-dirs --output /opt/slurm/etc/scripts/prolog.d/80_cloudwatch_agent_config_prolog.sh --create-file-mode 0755 https://raw.githubusercontent.com/openbraininstitute/hpc-resource-provisioner/refs/heads/slurm_prolog/provisioner_scripts/headnode/80_cloudwatch_agent_config_prolog.sh
