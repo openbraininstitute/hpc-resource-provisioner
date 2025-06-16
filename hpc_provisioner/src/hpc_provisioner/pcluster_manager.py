bf93f961d3bf4fd56abb9a514088f22bb876c425
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-06-16 15:22:16 +0200

    bucket prefix

--- hpc_provisioner/src/hpc_provisioner/pcluster_manager.py
+++ hpc_provisioner/src/hpc_provisioner/pcluster_manager.py
@@ -95,9 +95,9 @@ def populate_config(
         CONFIG_VALUES["create_users_args"] = create_users_args
     CONFIG_VALUES["environment_args"] = [cluster.name]
     CONFIG_VALUES["ami_id"] = get_ami_id()
-    CONFIG_VALUES["infra_assets_bucket"] = get_infra_bucket()
-    CONFIG_VALUES["create_users_script"] = f"s3://{get_infra_bucket()}/scripts/create_users.py"
-    CONFIG_VALUES["environment_script"] = f"s3://{get_infra_bucket()}/scripts/environment.sh"
+    CONFIG_VALUES["infra_assets_bucket"] = get_infra_bucket().lstrip("s3://")
+    CONFIG_VALUES["create_users_script"] = f"{get_infra_bucket()}/scripts/create_users.py"
+    CONFIG_VALUES["environment_script"] = f"{get_infra_bucket()}/scripts/environment.sh"
     logger.debug(f"Config values: {CONFIG_VALUES}")
 
 
