619157f678b5bcaf6a87cf15c006a9738ed403e0
Erik Heeren <erik.heeren@openbraininstitute.org>
2025-06-18 14:05:37 +0200

    Claim cluster + fixes for get unclaimed clusters

--- hpc_provisioner/src/hpc_provisioner/cluster.py
+++ hpc_provisioner/src/hpc_provisioner/cluster.py
@@ -2,6 +2,8 @@ import copy
 from json import JSONEncoder
 from typing import Optional
 
+BOOL_VALUES = ["benchmark", "dev", "include_lustre", "claimed"]
+
 
 class ClusterJSONEncoder(JSONEncoder):
     def default(self, o):
@@ -17,6 +19,7 @@ class Cluster:
     admin_ssh_key_name: Optional[str]
     tier: str
     vlab_id: str
+    claimed: bool
 
     def __init__(
         self,
@@ -28,6 +31,7 @@ class Cluster:
         include_lustre: bool = True,
         sim_pubkey: Optional[str] = None,
         admin_ssh_key_name: Optional[str] = None,
+        claimed: bool = False,
     ):
         self.benchmark = benchmark
         self.dev = dev
@@ -40,6 +44,7 @@ class Cluster:
             self.admin_ssh_key_name = admin_ssh_key_name
         else:
             self.admin_ssh_key_name = self.name
+        self.claimed = claimed
 
     @property
     def name(self):
@@ -66,6 +71,28 @@ class Cluster:
     def as_dict(self) -> dict:
         d = copy.deepcopy(self.__dict__)
         d["name"] = self.name
+        return d
+
+    @staticmethod
+    def from_dynamo_dict(cluster_data: dict):
+        """
+        Create the Cluster based on a dict retrieved from dynamodb,
+        which means converting the boolean values back from 1 and 0 to True and False
+        """
+        for bool_value in BOOL_VALUES:
+            cluster_data[bool_value] = True if cluster_data[bool_value] == 1 else False
+
+        return Cluster.from_dict(cluster_data)
+
+    def as_dynamo_dict(self) -> dict:
+        """
+        Return a dict where all booleans have been replaced with 1 (True) or 0 (False)
+        This way we can create GSIs for them in dynamo if necessary
+        """
+        d = self.as_dict()
+        for bool_value in BOOL_VALUES:
+            d[bool_value] = 1 if d[bool_value] else 0
+
         return d
 
     def __eq__(self, other) -> bool:
