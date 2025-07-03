b8e1faf7edd8fb1c9005df7b026b0f4511a7747f
Judit Planas <judit.planas@openbraininstitute.org>
2025-07-03 10:48:38 +0200

    Update user scripts according to the new provisioner behavior.

--- user_scripts/cluster_down.sh
+++ user_scripts/cluster_down.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-## This script:
+## Given a cluster name (<VLAB_ID>_<PROJECT_ID>), this script:
 ## 1. Calls HPC provisioner to delete the given cluster
 ## 2. Deletes the SSH key file from the bastion host (assuming this was copied by cluster_status script)
 ## 3. Deletes the local copy of the SSH key file
 ##
 ## Notes:
 ## - This script assumes the following:
-##     - The cluster with <PROJECT_ID> exists
+##     - The cluster with <cluster_name> exists
 ##     - The sequence of cluster_up + cluster_status + cluster_down scripts are called in this order
 ## - This script does NOT check for every possible error
 
 
-# PROJECT_ID needs to be passed to the script
+# Cluster name needs to be passed to the script
 if [ -z "$1" ]; then
-  echo "Usage: $0 <PROJECT_ID>"
+  echo "Usage: $0 <cluster_name>"
   exit 1
 fi
 
@@ -23,41 +23,62 @@ export AWS_ACCESS_KEY_ID=$(awk '/^aws_access_key_id/ {print $3}' ~/.aws/credenti
 export AWS_SECRET_ACCESS_KEY=$(awk '/^aws_secret/ {print $3}' ~/.aws/credentials)
 export AWS_APIGW_DEPLOY_ID=""
 export AWS_REGION="us-east-1"
-export PROJECT_ID="$1"
-export VLAB_ID=$USER
-export SSH_KEY_FILE=${PROJECT_ID}
+
+# Get the VLAB ID and the Project ID from the cluster name
+IFS='_' read -r -a ARG <<< "$1"
+export VLAB_ID="${ARG[0]}"
+export PROJECT_ID="${ARG[1]}"
+
+export SSH_KEY_FILE=$1
 
 # Delete the given cluster
-echo "Calling cluster deletion of ${PROJECT_ID}"
-export COMMAND="curl -X DELETE --user \""${AWS_ACCESS_KEY_ID}":"${AWS_SECRET_ACCESS_KEY}"\" --aws-sigv4 \"aws:amz:"${AWS_REGION}":execute-api\" https://"${AWS_APIGW_DEPLOY_ID}".execute-api."${AWS_REGION}".amazonaws.com/production/hpc-provisioner/pcluster\?project_id\="${PROJECT_ID}"\&vlab_id\="${VLAB_ID}" | jq"
-echo "+ ${COMMAND}"
+echo "Calling the deletion of cluster '${VLAB_ID}_${PROJECT_ID}'"
+export COMMAND="curl -X DELETE --user \""${AWS_ACCESS_KEY_ID}":"${AWS_SECRET_ACCESS_KEY}"\" --aws-sigv4 \"aws:amz:"${AWS_REGION}":execute-api\" https://"${AWS_APIGW_DEPLOY_ID}".execute-api."${AWS_REGION}".amazonaws.com/production/hpc-provisioner/pcluster\?project_id\="${PROJECT_ID}"\&vlab_id\="${VLAB_ID}
+echo "+ ${COMMAND} | jq"
 export CLUSTER=$(eval "${COMMAND}")
 echo "${CLUSTER}" | jq
 
-# Delete the SSH key file from the bastion host
-if ssh ec2-user@107.22.159.90 test -f "~/.ssh/${SSH_KEY_FILE}"
+# Check for errors: if 'message' field is present, there's an error
+export ERROR=$(echo ${CLUSTER} | jq -r '.message')
+if [ $ERROR != "null" ]
 then
-	ssh ec2-user@107.22.159.90 "rm ~/.ssh/${SSH_KEY_FILE}"
-	ssh ec2-user@107.22.159.90 "rm ~/.ssh/${SSH_KEY_FILE}_admin"
+    echo "Error in cluster deletion:"
+    echo "${CLUSTER}" | jq
+    # Don't exit yet, as we will clean up potentially left ARN key files
+fi
+
+# Bastion host IP
+export BASTION_IP="98.84.40.226"
+
+# Delete the SSH key file from the bastion host
+if ssh ec2-user@${BASTION_IP} test -f "~/.ssh/${SSH_KEY_FILE}"
+then
+	ssh ec2-user@${BASTION_IP} "rm ~/.ssh/${SSH_KEY_FILE}"
+	ssh ec2-user@${BASTION_IP} "rm ~/.ssh/${SSH_KEY_FILE}.admin"
 	echo "SSH keyfiles deleted from bastion host"
-	export IP_ADDR=$(cat ${SSH_KEY_FILE}_ip)
-	ssh ec2-user@107.22.159.90 "ssh-keygen -R ${IP_ADDR}"
-	rm -rf ${SSH_KEY_FILE}_ip
+	export IP_ADDR=$(cat ${SSH_KEY_FILE}.ip)
+	ssh ec2-user@${BASTION_IP} "ssh-keygen -R ${IP_ADDR}"
 	echo "Head node keys deleted from bastion host's known_hosts"
 fi
 
 # Delete the local copy of the SSH key file
 rm -f ${SSH_KEY_FILE}
-rm -f ${SSH_KEY_FILE}_admin
-echo "SSH keyfile deleted locally" 
+rm -f ${SSH_KEY_FILE}.admin
+rm -rf ${SSH_KEY_FILE}.ip
+echo "SSH keyfiles deleted locally" 
 
-# Check that the cluster is being deleted
-export IS_DELETED=$(echo $CLUSTER | jq -r '.cluster | .clusterStatus')
-
-if [ $IS_DELETED = "DELETE_IN_PROGRESS" ]
+# If an error was detected above, exit; otherwise, check that the cluster is being deleted
+if [ $ERROR != "null" ]
 then
-	echo "Cluster ${PROJECT_ID} is shutting down"
+	exit 1
 else
-	echo "Cluster ${PROJECT_ID} deleted"
+	export IS_DELETED=$(echo $CLUSTER | jq -r '.cluster | .clusterStatus')
+
+	if [ $IS_DELETED = "DELETE_IN_PROGRESS" ]
+	then
+		echo "Cluster '${VLAB_ID}_${PROJECT_ID}' is shutting down"
+	else
+		echo "Cluster '${VLAB_ID}_${PROJECT_ID}' has been deleted"
+	fi
 fi
 
