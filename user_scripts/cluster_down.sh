d84487c75aea8f1d75d5d01a1d3faadf04ca61c4
Judit <jplanasc@users.noreply.github.com>
2025-08-11 14:09:43 +0200

    Update user scripts according to the new provisioner behavior. (#63)
    * Update user scripts according to the new provisioner behavior.
    
    * Create an extra file with cluster configuration for better handling
    
    * Echo cluster list string without jq command as well

--- user_scripts/cluster_down.sh
+++ user_scripts/cluster_down.sh
@@ -1,63 +1,88 @@
 #!/bin/bash
 
-## This script:
+## Given a cluster name (<VLAB_ID>_<PROJECT_ID>), this script:
 ## 1. Calls HPC provisioner to delete the given cluster
 ## 2. Deletes the SSH key file from the bastion host (assuming this was copied by cluster_status script)
 ## 3. Deletes the local copy of the SSH key file
 ##
 ## Notes:
 ## - This script assumes the following:
-##     - The cluster with <PROJECT_ID> exists
+##     - The cluster with <cluster_name> exists
 ##     - The sequence of cluster_up + cluster_status + cluster_down scripts are called in this order
 ## - This script does NOT check for every possible error
 
 
-# PROJECT_ID needs to be passed to the script
+# Cluster name needs to be passed to the script
 if [ -z "$1" ]; then
-  echo "Usage: $0 <PROJECT_ID>"
+  echo "Usage: $0 <cluster_name>"
   exit 1
 fi
+
+export CFG_FILE="$1.config"
 
 # AWS credentials and needed data
 export AWS_ACCESS_KEY_ID=$(awk '/^aws_access_key_id/ {print $3}' ~/.aws/credentials)
 export AWS_SECRET_ACCESS_KEY=$(awk '/^aws_secret/ {print $3}' ~/.aws/credentials)
-export AWS_APIGW_DEPLOY_ID=""
-export AWS_REGION="us-east-1"
-export PROJECT_ID="$1"
-export VLAB_ID=$USER
-export SSH_KEY_FILE=${PROJECT_ID}
+export AWS_APIGW_DEPLOY_ID="$(cat ${CFG_FILE} | jq -r '.aws | .aws_api_gw')" #"033upmtu0d"
+export AWS_REGION="$(cat ${CFG_FILE} | jq -r '.aws | .aws_region')"
+
+# Get the VLAB ID and the Project ID from the config file
+export VLAB_ID="$(cat ${CFG_FILE} | jq -r '.config | .vlab_id')"
+export PROJECT_ID="$(cat ${CFG_FILE} | jq -r '.config | .project_id')"
+
+export SSH_KEY_FILE="$(cat ${CFG_FILE} | jq -r '.aws | .ssh_key_file')"
+export SSH_ADMIN_FILE="$(cat ${CFG_FILE} | jq -r '.aws | .ssh_admin_file')"
 
 # Delete the given cluster
-echo "Calling cluster deletion of ${PROJECT_ID}"
-export COMMAND="curl -X DELETE --user \""${AWS_ACCESS_KEY_ID}":"${AWS_SECRET_ACCESS_KEY}"\" --aws-sigv4 \"aws:amz:"${AWS_REGION}":execute-api\" https://"${AWS_APIGW_DEPLOY_ID}".execute-api."${AWS_REGION}".amazonaws.com/production/hpc-provisioner/pcluster\?project_id\="${PROJECT_ID}"\&vlab_id\="${VLAB_ID}" | jq"
-echo "+ ${COMMAND}"
+echo "Calling the deletion of cluster '${VLAB_ID}_${PROJECT_ID}'"
+export COMMAND="curl -X DELETE --user \""${AWS_ACCESS_KEY_ID}":"${AWS_SECRET_ACCESS_KEY}"\" --aws-sigv4 \"aws:amz:"${AWS_REGION}":execute-api\" https://"${AWS_APIGW_DEPLOY_ID}".execute-api."${AWS_REGION}".amazonaws.com/production/hpc-provisioner/pcluster\?project_id\="${PROJECT_ID}"\&vlab_id\="${VLAB_ID}
+echo "+ ${COMMAND} | jq"
 export CLUSTER=$(eval "${COMMAND}")
+echo ${CLUSTER}
+
+# Check for errors: if 'message' field is present, there's an error
+export ERROR=$(echo ${CLUSTER} | jq -r '.message')
+if [ $ERROR != "null" ]
+then
+	echo "Error in cluster deletion:"
+	# Don't exit yet, as we will clean up potentially left ARN key files
+fi
+
 echo "${CLUSTER}" | jq
 
+# Bastion host IP
+export BASTION_IP="98.84.40.226"
+
 # Delete the SSH key file from the bastion host
-if ssh ec2-user@107.22.159.90 test -f "~/.ssh/${SSH_KEY_FILE}"
+if ssh ec2-user@${BASTION_IP} test -f "~/.ssh/${SSH_KEY_FILE}"
 then
-	ssh ec2-user@107.22.159.90 "rm ~/.ssh/${SSH_KEY_FILE}"
-	ssh ec2-user@107.22.159.90 "rm ~/.ssh/${SSH_KEY_FILE}_admin"
+	ssh ec2-user@${BASTION_IP} "rm ~/.ssh/${SSH_KEY_FILE}"
+	ssh ec2-user@${BASTION_IP} "rm ~/.ssh/${SSH_ADMIN_FILE}"
 	echo "SSH keyfiles deleted from bastion host"
-	export IP_ADDR=$(cat ${SSH_KEY_FILE}_ip)
-	ssh ec2-user@107.22.159.90 "ssh-keygen -R ${IP_ADDR}"
-	rm -rf ${SSH_KEY_FILE}_ip
+	export IP_ADDR=$(cat ${SSH_KEY_FILE}.ip)
+	ssh ec2-user@${BASTION_IP} "ssh-keygen -R ${IP_ADDR}"
 	echo "Head node keys deleted from bastion host's known_hosts"
 fi
 
 # Delete the local copy of the SSH key file
 rm -f ${SSH_KEY_FILE}
-rm -f ${SSH_KEY_FILE}_admin
-echo "SSH keyfile deleted locally" 
+rm -f ${SSH_ADMIN_FILE}
+rm -rf ${SSH_KEY_FILE}.ip
+rm -rf ${SSH_KEY_FILE}.config
+echo "SSH keyfiles deleted locally" 
 
-# Check that the cluster is being deleted
-export IS_DELETED=$(echo $CLUSTER | jq -r '.cluster | .clusterStatus')
-
-if [ $IS_DELETED = "DELETE_IN_PROGRESS" ]
+# If an error was detected above, exit; otherwise, check that the cluster is being deleted
+if [ $ERROR != "null" ]
 then
-	echo "Cluster ${PROJECT_ID} is shutting down"
+	exit 1
 else
-	echo "Cluster ${PROJECT_ID} deleted"
+	export IS_DELETED=$(echo $CLUSTER | jq -r '.cluster | .clusterStatus')
+
+	if [ $IS_DELETED = "DELETE_IN_PROGRESS" ]
+	then
+		echo "Cluster '${VLAB_ID}_${PROJECT_ID}' is shutting down"
+	else
+		echo "Cluster '${VLAB_ID}_${PROJECT_ID}' has been deleted"
+	fi
 fi
 
--- user_scripts/cluster_up.sh
+++ user_scripts/cluster_up.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 ## This script:
-## 1. Calls HPC provisioner to create a cluster with the <PROJECT_ID> defined in the variable below
+## 1. Calls HPC provisioner to create a cluster with the 'cluster name' defined in the variables below
 ## 2. Calls 'aws secretsmanager' to get the SSH key file of the new cluster
 ##
-## For simplicity, the SSH key filename is the name of the cluster (<PROJECT_ID>)
+## For simplicity, the SSH key filename is the name of the cluster (<VLAB_ID>_<PROJECT_ID>)
 ##
 ## Notes:
 ## - This script assumes the following:
 ##     - The sequence of cluster_up + cluster_status + cluster_down scripts are called in this order
 ## - This script does NOT check for every possible error
 ## - This script allows tuning the cluster through some variables
+## - If a cluster name is passed as a parameter, this variable is used instead of creating a new name
+##     - VLAB_ID and PROJECT_ID can't contain "_", so the underscore is a valid delimiter to later recover
+##       them from the cluster name
 
 
 # AWS credentials and needed data
@@ -19,12 +22,20 @@ export AWS_SECRET_ACCESS_KEY=$(awk '/^aws_secret/ {print $3}' ~/.aws/credentials
 export AWS_APIGW_DEPLOY_ID=""
 export AWS_REGION="us-east-1"
 
-# PROJECT_ID, with automatic creation date for easy identification and uniqueness (date format is YYYY-MM-DD-hh-mm)
-export PROJECT_ID="$(date '+%Y-%m-%d-%Hh%M')"
-export SSH_KEY_FILE=${PROJECT_ID}
+# Default cluster name (VLAB_ID and PROJECT_ID), with automatic creation date for easy identification and uniqueness
+# Date format is YYYY-MM-DD-hh-mm
+export VLAB_ID="$(date '+%Y-%m-%d-%Hh%M')"
+export PROJECT_ID="my-project"
 
-#VLAB_ID set to the username
-export VLAB_ID=${USER}
+# Overwrite default values if an argument is passed to the script
+if [ "$1" ]; then
+	# Get the VLAB ID and the Project ID from the cluster name
+	IFS='_' read -r -a ARG <<< "$1"
+	export VLAB_ID="${ARG[0]}"
+	export PROJECT_ID="${ARG[1]}"
+fi
+
+export SSH_KEY_FILE="${VLAB_ID}_${PROJECT_ID}"
 
 # Cluster custom parameters
 ## Mount FSx Lustre storage
@@ -35,7 +46,7 @@ export BENCHMARK="false"
 export TIER="mixed-prod-mpi" # c7a and c6a
 
 # Cluster creation call
-echo "Calling cluster creation of ${PROJECT_ID}"
+echo "Calling the creation of cluster '${VLAB_ID}_${PROJECT_ID}'"
 export COMMAND="curl -X POST --user \""${AWS_ACCESS_KEY_ID}":"${AWS_SECRET_ACCESS_KEY}"\" --aws-sigv4 \"aws:amz:"${AWS_REGION}":execute-api\" https://"${AWS_APIGW_DEPLOY_ID}".execute-api."${AWS_REGION}".amazonaws.com/production/hpc-provisioner/pcluster\?"
 
 # Parameters need to go alphabetically ordered
@@ -48,37 +59,65 @@ if [ "${LUSTRE}" = "false" ]; then
     export COMMAND=${COMMAND}"include_lustre\=false\&"
 fi
 
-export COMMAND=${COMMAND}"project_id\="${PROJECT_ID}"\&tier\="${TIER}"\&vlab_id\="${VLAB_ID}" | jq"
-echo "+ ${COMMAND}"
+export COMMAND=${COMMAND}"project_id\="${PROJECT_ID}"\&tier\="${TIER}"\&vlab_id\="${VLAB_ID}
+echo "+ ${COMMAND} | jq"
 export CLUSTER=$(eval "${COMMAND}")
 
-echo "Cluster creation requested with ID '${PROJECT_ID}'"
+echo "Cluster creation requested with cluster name: '${VLAB_ID}_${PROJECT_ID}'"
+
+# Check for creation errors: if 'message' field is present, there's an error
+export ERROR=$(echo ${CLUSTER} | jq -r '.message')
+if [ $ERROR != "null" ]
+then
+	echo "Error creating cluster:"
+	echo "${CLUSTER}" | jq
+	exit 1
+fi
+
+# Cluster creation request succeeded beyond this point
 echo "Cluster info:"
 echo "${CLUSTER}" | jq
 
 # Create SSH key file
-touch ${PROJECT_ID}
-chmod 0600 ${PROJECT_ID}
+touch ${SSH_KEY_FILE}
+chmod 0600 ${SSH_KEY_FILE}
 export CLUSTER_SSH_KEY_ARN=$(echo ${CLUSTER} | jq -r '.cluster.private_ssh_key_arn')
 if [ $CLUSTER_SSH_KEY_ARN = "null" ]
 then
-	echo "Error: could not get the cluster SSH key file"
+	echo "Error: could not get the cluster SSH key arn"
 else
 	echo "Getting arn key for: ${CLUSTER_SSH_KEY_ARN}"
 	aws secretsmanager get-secret-value --secret-id=${CLUSTER_SSH_KEY_ARN} | jq -r .SecretString >| ${SSH_KEY_FILE}
 	echo "Secret key stored in ${SSH_KEY_FILE}"
 fi
 
-export ADMIN_KEY=${PROJECT_ID}_admin
-touch $ADMIN_KEY
-chmod 0600 ${ADMIN_KEY}
+export ADMIN_KEY_FILE="${SSH_KEY_FILE}.admin"
+touch $ADMIN_KEY_FILE
+chmod 0600 ${ADMIN_KEY_FILE}
 export CLUSTER_SSH_KEY_ARN_ADMIN=$(echo ${CLUSTER} | jq -r '.cluster.admin_user_private_ssh_key_arn')
 if [ $CLUSTER_SSH_KEY_ARN_ADMIN = "null" ]
 then
-	echo "Error: could not get the cluster admin SSH key file"
+	echo "Error: could not get the cluster admin SSH key arn"
 else
 	echo "Getting arn admin key for: ${CLUSTER_SSH_KEY_ARN_ADMIN}"
-	aws secretsmanager get-secret-value --secret-id=${CLUSTER_SSH_KEY_ARN_ADMIN} | jq -r .SecretString >| ${ADMIN_KEY}
-	echo "Secret key stored in ${ADMIN_KEY}"
+	aws secretsmanager get-secret-value --secret-id=${CLUSTER_SSH_KEY_ARN_ADMIN} | jq -r .SecretString >| ${ADMIN_KEY_FILE}
+	echo "Secret key stored in ${ADMIN_KEY_FILE}"
 fi
+
+export CFG_FILE="${SSH_KEY_FILE}.config"
+echo "{" > ${CFG_FILE}
+echo "  \"config\": {" >> ${CFG_FILE}
+echo "    \"vlab_id\": \"${VLAB_ID}\"," >> ${CFG_FILE}
+echo "    \"project_id\": \"${PROJECT_ID}\"," >> ${CFG_FILE}
+echo "    \"lustre\": ${LUSTRE}," >> ${CFG_FILE}
+echo "    \"benchmark\": ${BENCHMARK}," >> ${CFG_FILE}
+echo "    \"tier\": \"${TIER}\"" >> ${CFG_FILE}
+echo "  }," >> ${CFG_FILE}
+echo "  \"aws\": {" >> ${CFG_FILE}
+echo "    \"aws_api_gw\": \"${AWS_APIGW_DEPLOY_ID}\"," >> ${CFG_FILE}
+echo "    \"aws_region\": \"${AWS_REGION}\"," >> ${CFG_FILE}
+echo "    \"ssh_key_file\": \"${SSH_KEY_FILE}\"," >> ${CFG_FILE}
+echo "    \"ssh_admin_file\": \"${ADMIN_KEY_FILE}\"" >> ${CFG_FILE}
+echo "  }" >> ${CFG_FILE}
+echo "}" >> ${CFG_FILE}
 
